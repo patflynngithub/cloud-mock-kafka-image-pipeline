aec799434ea49a96300aabdedda787085b377b02
Patrick Flynn <patflynn@hotmail.com>
2026-03-09 22:19:42 -0700

    Update image event alert client's error/exception/retry code

--- image_event_alert_client.py
+++ image_event_alert_client.py
@@ -19,11 +19,93 @@ import mysql.connector
 from mysql.connector import Error
 
 from CONSTANTS.CONSTANTS import *
-
 # relational database and object storage access info
 from CLOUD_INFO.CLOUD_INFO import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
 # ===================================================================================================
+
+def add_image_event_alert(image_event_id):
+    """
+    Stores new image event alert in the image event alert database table. 
+    Returns the image_event_alert id number (primary key) automatically
+    generated when doing this.
+    """
+
+    global cursor
+
+    # this will contain the primary key's unique integer value automatically generated
+    # when adding the image event alert to the image event alert database table
+    image_event_alert_id = -1
+
+    attempt  = 1
+    attempts = 3
+    delay    = 2
+    # loop is for retrying when "retryable" cursor.execute() error happens
+    while attempt <= attempts:
+    
+        try:
+            # if the connection is lost, attempt to reconnect
+            if not rdb_connection.is_connected():
+                rdb_connection.reconnect()
+                cursor = rdb_connection.cursor()
+
+            # Add new image event alert to the image event alert database table
+
+            print(f"Adding new image event alert (for image event #{image_event_id}) to the image event alert database table")
+            add_image_event_alert_query = "INSERT INTO image_event_alert (image_event_id) VALUES (%s)"
+            query_data                    = (image_event_id,)
+            print(add_image_event_alert_query)
+            print(f"data = {query_data}")
+            cursor.execute(add_image_event_alert_query, query_data)
+            break
+
+        # for this error that is out of the programmer's control, 
+        # will retry mulitple times with increasing delay to insert data into database table
+        except mysql.connector.OperationalError as err:
+            print(f"Operational Error when adding image event alert to image event alert table")
+            logging.error(err)
+            if attempt == attempts:
+                print(f"Failed after {attempts} attempts")
+                # will exit the program and print a traceback
+                raise RuntimeError("A fatal run-time error occurred. Exiting with traceback.")
+            print(f"Retrying ({attempt}/{attempts})...")
+            time.sleep(delay ** (attempt-1)) # Exponential backoff
+            attempt += 1
+            continue
+
+        # non-retryable error
+        except mysql.connector.Error as err:
+            print(f"Error when adding image event alert to image event alert table")
+            logging.error(err)
+            # will exit the program and print a traceback
+            raise RuntimeError("A fatal run-time error occurred. Exiting with traceback.")
+
+    try:
+        rdb_connection.commit()
+
+        # retrieve the integer just automatically generated for the new row's
+        # primary key image_id column in the image event alert table
+        image_event_alert_id = cursor.lastrowid
+        print (f"Added new image event alert to the image event alert table. New image event alert ID# is {image_event_alert_id}")
+
+    except Error as e:
+        print("Error committing image event alert to the image event alert table")
+        logging.error(e)
+
+        rdb_connection.rollback() # Roll back the INSERT transaction
+
+        if cursor:
+            cursor.close()
+        if rdb_connection and rdb_connection.is_connected():
+            rdb_connection.close()
+            print("Database connection closed.")
+
+        # will exit the program and print a traceback
+        raise RuntimeError("A fatal run-time error occurred. Exiting with traceback.")
+
+    return image_event_alert_id
+
+# ---------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
@@ -75,19 +157,7 @@ if __name__ == "__main__":
         print(f"Image event ID = {image_event_id}")
 
         # Add new image event alert to the image event alert relational database table
-
-        print(f"Adding new image event alert to the image event alert database table")
-        add_image_event_alert_query = f"INSERT INTO image_event_alert (image_event_id) VALUES ('{image_event_id}')"
-        print(add_image_event_alert_query)
-        cursor.execute(add_image_event_alert_query)
-        rdb_connection.commit()
-
-        # get the image event alert ID integer just now automatically
-        # generated when adding the new image event alert to the image event
-        # alert database table.
-        image_event_alert_id = cursor.lastrowid
-        print(f"New image event alert ID = {image_event_alert_id}")
-        print()
+        add_image_event_alert(image_event_id)
 
         # HERE, EMAIL ALERTS WOULD BE SENT TO THOSE WHO HAVE SUBSCRIBED TO BE
         # ALERTED ABOUT THIS TYPE OF IMAGE EVENT
